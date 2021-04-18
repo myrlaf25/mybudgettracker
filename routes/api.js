222b8a45716f08ec2f59025898059085ce0f340d
Myrla <75264586+myrlaf25@users.noreply.github.com>
2021-04-18 15:42:40 -0700

    addind path

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
+const path = require("path");
+
 
 router.post("/api/transaction", ({body}, res) => {
   Transaction.create(body)
