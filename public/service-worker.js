7cbb7ff762162bdd5186565fc46f9bbd4952ce4f
Myrla <75264586+myrlaf25@users.noreply.github.com>
2021-04-19 09:55:28 -0700

    changes

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,11 +4,11 @@ const FILES_TO_CACHE = [
     './',
     './index.html',
     './styles.css',
-    './icons/icon-192x192.png',
-    './icons/icon-512x512.png',
+    '../icons/icon-192x192.png',
+    '../icons/icon-512x512.png',
     './db.js',
     './index.js',
-    
+
 ];
 //  install event handler
 // install
