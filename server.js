f264e470c3f3d77b28fef4514a158cc5947390c0
Myrla <75264586+myrlaf25@users.noreply.github.com>
2021-04-19 15:12:54 -0700

    changes

--- server.js
+++ server.js
@@ -26,5 +26,5 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budgetdb", {
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+    console.log(`App running on port ${PORT}!`);
 });
